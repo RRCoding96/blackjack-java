11943e14616750fbff5a498532f578094fb4e937
Reznov <reznov03048@gamil.com>
2022-12-15 21:51:04 +0900

    feat: gamer가 받은 카드를 확인하는 기능 구현

--- src/Gamer.java
+++ src/Gamer.java
@@ -3,11 +3,24 @@ import java.util.List;
 public class Gamer {
     private List<Card> cards;
 
-    public void receiveCard(Card card){
+    public void receiveCard(Card card) {
         this.cards.add(card);
+        showCards();
+    }
+
+    public void showCards() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("현재 보유 카드 목록 \n");
+
+        for (Card card : cards) {
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
     }
 
     public List<Card> openCards() {
-        return null;
+        return this.cards;
     }
 }
