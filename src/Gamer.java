870bec3c558f3236e91d644876b97d881a3ea48b
Reznov <reznov03048@gamil.com>
2022-12-15 21:48:47 +0900

    feat: gamer가 카드를 받는 기능 구현

--- src/Gamer.java
+++ src/Gamer.java
@@ -3,7 +3,9 @@ import java.util.List;
 public class Gamer {
     private List<Card> cards;
 
-    public void receiveCard(Card card){}
+    public void receiveCard(Card card){
+        this.cards.add(card);
+    }
 
     public List<Card> openCards() {
         return null;
