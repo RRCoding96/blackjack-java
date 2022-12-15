6d62d12cba4064b585711f8b70f3c33ac482bdbb
Reznov <reznov03048@gamil.com>
2022-12-15 21:10:26 +0900

    feat: Game 생성

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+public class Game {
+
+    public void play() {
+        System.out.printf("========= Blackjack ===========");
+        Dealer dealer = new Dealer();
+        Gamer gamer = new Gamer();
+        Rule rule = new Rule();
+        CardDeck cardDeck = new CardDeck();
+    }
+}
