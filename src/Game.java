af4920d2f8d3e9935ee7bdd4b61afd2f983e794e
Reznov <reznov03048@gamil.com>
2022-12-15 21:59:06 +0900

    feat: 처음에 2장의 카드를 gamer에게 주는 기능 구현

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Game {
+    private static final int INIT_RECEIVE_CARD_COUNT = 2;
 
     public void play() {
         System.out.printf("========= Blackjack ===========");
@@ -11,7 +12,16 @@ public class Game {
         Rule rule = new Rule();
         CardDeck cardDeck = new CardDeck();
 
+        initPhase(cardDeck, gamer);
         playingPhase(sc, cardDeck, gamer);
+    }
+
+    private void initPhase(CardDeck cardDeck, Gamer gamer) {
+        System.out.println("처음 2장의 카드를 각자 뽑겠습니다.");
+        for (int i = 0; i < INIT_RECEIVE_CARD_COUNT; i++) {
+            Card card = cardDeck.draw();
+            gamer.receiveCard(card);
+        }
     }
 
     private void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer) {
@@ -20,7 +30,7 @@ public class Game {
             System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
             gamerInput = sc.nextLine();
 
-            if("0".equals(gamerInput)) break;
+            if ("0".equals(gamerInput)) break;
 
             Card card = cardDeck.draw();
             gamer.receiveCard(card);
