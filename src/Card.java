0181d2dd8a686bd4aaba3d2c2a6d22e43ba9e043
Reznov <reznov03048@gamil.com>
2022-12-15 21:31:15 +0900

    feat: Card의 생성자 구현

--- src/Card.java
+++ src/Card.java
@@ -2,6 +2,11 @@ public class Card {
     private String pattern; // 무늬
     private String denomination; // 끗수
 
+    public Card(String pattern, String denomination) {
+        this.pattern = pattern;
+        this.denomination = denomination;
+    }
+
     public String getPattern() {
         return pattern;
     }
